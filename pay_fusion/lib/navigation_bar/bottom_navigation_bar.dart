a90c2439a5fca0e6b2fb0554e6d17483b83b2b0c
AzhanZeni <azhanhaniff02@gmail.com>
2025-07-18 21:44:23 +0800

    Add QR payment and receive pages
    Introduces new pages for QR payment, pay with QR, receive via QR, and QR scan functionality.
    
    Updates the bottom navigation bar to integrate QR scan page and refines navigation logic.

--- pay_fusion/lib/navigation_bar/bottom_navigation_bar.dart
+++ pay_fusion/lib/navigation_bar/bottom_navigation_bar.dart
@@ -4,6 +4,7 @@ import 'package:pay_fusion/main.dart';
 import 'package:pay_fusion/pages/ekyc_page.dart';
 import 'package:pay_fusion/pages/home_page_bank.dart';
 import 'package:pay_fusion/pages/home_page_card.dart';
+import 'package:pay_fusion/pages/qr/qr_scan_page.dart';
 
 class UserNavigationBar extends StatefulWidget {
   const UserNavigationBar({super.key});
@@ -16,12 +17,11 @@ class _UserNavigationBarState extends State<UserNavigationBar> {
   int _selectedIndex = 0;
 
   static final List<Widget> _pages = <Widget>[
-    
     currentView == CurrentViewEnum.user ? HomePageCard() : HomePageBank(),
 
     //update your pages here
-    Center(child: Text('QR'),),
-    Center(child: Text('Tax or Invoice'),),
+    QRScanPage(),
+    Center(child: Text('Tax or Invoice')),
   ];
 
   void _onItemTapped(int index) {
@@ -29,6 +29,7 @@ class _UserNavigationBarState extends State<UserNavigationBar> {
       _selectedIndex = index;
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,12 +37,16 @@ class _UserNavigationBarState extends State<UserNavigationBar> {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           setState(() {
-        _selectedIndex = 1;
+            _selectedIndex = 1;
           });
         },
         shape: const CircleBorder(),
         backgroundColor: _selectedIndex == 1 ? Color(0xFF01BBB9) : Colors.white,
-        child:  Icon(Icons.qr_code_scanner, color: _selectedIndex == 1 ?  Colors.white : Colors.black , size: 30.0),
+        child: Icon(
+          Icons.qr_code_scanner,
+          color: _selectedIndex == 1 ? Colors.white : Colors.black,
+          size: 30.0,
+        ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: BottomNavigationBar(
@@ -52,18 +57,9 @@ class _UserNavigationBarState extends State<UserNavigationBar> {
         unselectedItemColor: Colors.grey,
         backgroundColor: Colors.black,
         items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: SizedBox.shrink(),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.article),
-            label: '',
-          ),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+          BottomNavigationBarItem(icon: SizedBox.shrink(), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.article), label: ''),
         ],
       ),
     );
